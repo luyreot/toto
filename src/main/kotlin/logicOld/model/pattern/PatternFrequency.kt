de047834661ea85fe0d6e3d973a9438b2babdb86
luyreot <luyreot@luyreot.com>
2022-10-09 20:27:47 +0300

    Delete old logic

--- src/main/kotlin/logicOld/model/pattern/PatternFrequency.kt
+++
@@ -1,9 +0,0 @@
-package logicOld.model.pattern
-
-import logicOld.data.Drawings
-
-/**
- * Holds a number, a frequency, which represents the number
- * between two subsequent occurrences of a [PatternNumeric] from [Drawings.drawings].
- */
-data class PatternFrequency(val frequency: Int) : PatternBase()
