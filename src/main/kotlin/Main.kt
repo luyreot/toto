9c9fb50064774496a869e899625cbcda1b9ab287
luyreot <luyreot@luyreot.com>
2023-02-12 17:27:08 +0200

    Update start issue index

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,7 +17,7 @@ object Main {
         PredictionTester.apply {
             isTestingPredictions = false
             startYear = 2023
-            startIssue = 4
+            startIssue = 11
 
             do {
                 println("======== 5x35 ========")
