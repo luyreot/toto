f508623c2fc837a732b201680282177eb26cb7a9
luyreot <luyreot@luyreot.com>
2024-12-15 14:07:40 +0200

    Move model classes in a separate package

--- src/main/kotlin/deeplearning/Neuron.kt
+++
@@ -1,9 +0,0 @@
-package deeplearning
-
-class Neuron(
-    val bias: Bias
-) {
-    constructor(bias: Int) : this(Bias(bias.toDouble()))
-    constructor(bias: Float) : this(Bias(bias.toDouble()))
-    constructor(bias: Double) : this(Bias(bias))
-}
--- src/main/kotlin/deeplearning/model/Weight.kt
+++ src/main/kotlin/deeplearning/model/Weight.kt
@@ -1,3 +1,3 @@
-package deeplearning
+package deeplearning.model
 
 class Weight(val weight: Double)
