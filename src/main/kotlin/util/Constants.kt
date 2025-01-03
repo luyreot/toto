882246536d2973505c69be75cced5e34af9d387d
luyreot <luyreot@luyreot.com>
2025-01-03 09:20:18 +0200

    Update drawings

--- src/main/kotlin/util/Constants.kt
+++ src/main/kotlin/util/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val PAGE_URL_5x35: String = "http://info.toto.bg/results/5x35/"
     const val PAGE_URL_6x42: String = "http://info.toto.bg/results/6x42/"
     const val PAGE_URL_6x49: String = "http://info.toto.bg/results/6x49/"
-    const val PAGE_YEAR: String = "2024"
+    const val PAGE_YEAR: String = "2025"
 
     const val PATH_TXT_6x49: String = "files/txt/6x49/"
     const val PATH_TXT_6x42: String = "files/txt/6x42/"
